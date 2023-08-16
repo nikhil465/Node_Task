5b973ae18dc310e9bd02cf3529444f04fe6b8cb7
Nikhil Lomate <lomatenikhil465@gmail.com>
2023-08-16 22:43:34 +0530

    changed jwt file

--- config/passport_jwt_strategy.js
+++ config/passport_jwt_strategy.js
@@ -3,28 +3,11 @@ const JWTStrategy = require("passport-jwt").Strategy;
 const ExtractJWT = require("passport-jwt").ExtractJwt;
 const env = require("./environment");
 
-const User = require("../models/user");
-
 let options = {
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: env.jwt_secret,
 };
 
-passport.use(
-  new JWTStrategy(options, function (jwtPayload, done) {
-    User.findById(jwtPayload._id)
-      .catch((err) => {
-        console.log("Error in finding user from JWT");
-        return;
-      })
-      .then((user) => {
-        if (user) {
-          return done(null, user);
-        } else {
-          return done(null, false);
-        }
-      });
-  })
-);
+passport.use(new JWTStrategy(options, function (jwtPayload, done) {}));
 
 module.exports = passport;
